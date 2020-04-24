c00bfef02103289680fac86ece03add585648553
Valentin Galabov <50594546+valentingalabov@users.noreply.github.com>
2020-04-24 21:06:27 +0300

    Add category test and channel test.

---
+++ Tests/PlayZone.Services.Data.Tests/CategoryTests.cs
@@ -0,0 +1,43 @@
+﻿namespace PlayZone.Services.Data.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using PlayZone.Data;
+    using PlayZone.Data.Models;
+    using PlayZone.Data.Repositories;
+    using PlayZone.Services.Mapping;
+    using PlayZone.Web.ViewModels.Videos;
+    using Xunit;
+
+    public class CategoryTests
+    {
+        [Fact]
+        public async Task GetAllCategoriesTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
+
+            var repository = new EfDeletableEntityRepository<Category>(new ApplicationDbContext(options.Options));
+
+            await repository.AddAsync(new Category()
+            {
+                Name = "Music",
+                Description = "music Description",
+            });
+
+            await repository.SaveChangesAsync();
+
+            AutoMapperConfig.RegisterMappings(typeof(CategoryDropDownViewModel).Assembly);
+
+            var service = new CategoriesService(repository);
+
+            var categories = service.GetAllCategories<CategoryDropDownViewModel>();
+
+            Assert.Equal("Music", categories.Select(c => c.Name).FirstOrDefault());
+        }
+    }
+}
---
+++ Tests/PlayZone.Services.Data.Tests/ChannelTests.cs
@@ -0,0 +1,49 @@
+﻿namespace PlayZone.Services.Data.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using CloudinaryDotNet;
+    using Microsoft.EntityFrameworkCore;
+    using PlayZone.Data;
+    using PlayZone.Data.Models;
+    using PlayZone.Data.Repositories;
+    using Xunit;
+
+    public class ChannelTests
+    {
+        [Fact]
+        public async Task IsValidChanelTest()
+        {
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
+
+            var channelRepository = new EfDeletableEntityRepository<Channel>(new ApplicationDbContext(options.Options));
+            var imageRepository = new EfDeletableEntityRepository<Image>(new ApplicationDbContext(options.Options));
+            var videoRepository = new EfDeletableEntityRepository<Video>(new ApplicationDbContext(options.Options));
+            var cloudinaryRepository = new Cloudinary(cloudinaryUrl: "channel test cloudinary");
+
+            await channelRepository.AddAsync(this.CreateChannel());
+
+            await channelRepository.SaveChangesAsync();
+
+            var service = new ChannelsService(channelRepository, imageRepository, videoRepository, cloudinaryRepository);
+
+            Assert.False(service.IsValidChannel("Enduro"));
+        }
+
+        public Channel CreateChannel()
+        {
+            return new Channel
+            {
+                Title = "Enduro",
+                Description = "Desc of channel",
+                Image = null,
+                UserId = null,
+            };
+        }
+
+
+    }
+}
